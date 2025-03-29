6b59e75784358eafc642c69843f2d3734bf4b525
Pravdomir Kostov <prkoso@softserveinc.com>
2025-03-29 22:15:59 +0200

    implement cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
     .setTitle('Sports Mate')
@@ -13,6 +15,23 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  const corsOrigins = configService.get<string>('CORS_ORIGINS')?.split(',');
+
+  app.enableCors({
+    origin: corsOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    //  HTTP headers that the frontend is allowed to send to API
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept', // For specifying response formats
+      'Origin', // For indicating request origin
+      'Access-Control-Allow-Origin',
+    ],
+    // custom headers that the backend sends which the frontend is able to read
+    exposedHeaders: ['Content-Range', 'X-Total-Count', 'X-Pagination-Pages'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
