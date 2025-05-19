ba08fb34802f103cde93993e4c4105d22b396468
Pravdomir Kostov <prkoso@softserveinc.com>
2025-05-20 01:19:06 +0300

    add logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard } from './guards/google-auth/google-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,13 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: { email: string; password: string }) {
     return this.authService.login(loginDto.email, loginDto.password);
+  }
+
+  // The client (browser/frontend) should delete this token from their storage
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  logout() {
+    return { message: 'Successfully logged out' };
   }
 
   @UseGuards(GoogleAuthGuard)
