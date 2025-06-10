3cff7876d8f7f893a730de853d43713c20955452
Pravdomir Kostov <prkoso@softserveinc.com>
2025-06-10 10:02:25 +0300

    fix import

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { City } from './entities/city.entity';
 import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([City])],
+  imports: [TypeOrmModule.forFeature([City]), CacheModule.register()],
   providers: [CitiesService],
   controllers: [CitiesController],
   exports: [CitiesService],
